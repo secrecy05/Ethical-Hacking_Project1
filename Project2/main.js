5a96e9ceaf155a2efc47e1a2ac21166721db6a32
Prince Goswami <cs2438.diatm@gmail.com>
2025-12-28 15:47:37 +0530

    Delete Project2 directory

--- Project2/main.js
+++
@@ -1,9 +0,0 @@
-// static/js/main.js
-document.addEventListener('DOMContentLoaded', ()=> {
-  const forms = document.querySelectorAll('form');
-  forms.forEach(f => f.addEventListener('submit', ()=> {
-    // simple submit visual feedback
-    const btn = f.querySelector('button[type="submit"]');
-    if (btn) btn.disabled = true;
-  }));
-});
